7d927fbd3f4a09749cf9c8a0a6c8f77c904c0863
alejandropascuale <apascuale@outlook.com>
2022-01-12 10:22:35 -0300

    solve problem 3

--- Ejercitacion1.sql
+++ Ejercitacion1.sql
@@ -7,3 +7,7 @@ GROUP BY estudiante_legajo
 SELECT estudiante_legajo, COUNT(*) AS 'Cantidad de Cursos' FROM INSCRIPCION
 WHERE (SELECT DISTINCT estudiante_legajo FROM INSCRIPCION)
 GROUP BY estudiante_legajo HAVING COUNT(*) > 1
+
+/* Escriba una consulta que devuelva la información de los estudiantes que no realizan ningún curso. */
+SELECT * FROM ESTUDIANTE
+WHERE legajo NOT IN (SELECT DISTINCT estudiante_legajo FROM INSCRIPCION)
