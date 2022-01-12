eeba862dc97068879341e1515a69dfdafd6fd3d4
alejandropascuale <apascuale@outlook.com>
2022-01-12 10:13:52 -0300

    solve problem 1 and 2

---
+++ Ejercitacion1.sql
@@ -0,0 +1,9 @@
+/* Escriba una consulta que devuelva el legajo y la cantidad de cursos que realiza cada estudiante. */
+SELECT estudiante_legajo, COUNT(*) AS 'Cantidad de Cursos' FROM INSCRIPCION
+WHERE (SELECT DISTINCT estudiante_legajo FROM INSCRIPCION)
+GROUP BY estudiante_legajo
+
+/* Escriba una consulta que devuelva el legajo y la cantidad de cursos de los estudiantes que realizan más de un curso. */
+SELECT estudiante_legajo, COUNT(*) AS 'Cantidad de Cursos' FROM INSCRIPCION
+WHERE (SELECT DISTINCT estudiante_legajo FROM INSCRIPCION)
+GROUP BY estudiante_legajo HAVING COUNT(*) > 1
